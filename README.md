33414c510b5b8031d881d88c4a1f683a4d1b8661
Aurélien Hébert <aurrelhebert@gmail.com>
2016-10-20 18:17:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# The WarpScript command
+
+## Config 
+
+Add in timelion properties (src/core_plugins/timelion/timelion.json) the Warp 10 URL backend:
+
+```
+  "warpscript": {
+    "url": "http://localhost:8080/api/v0/exec",
+    "header": "warp10"
+  },
+```
+
+Header corresponds to the pattern to match in error return (by default it's warp10). If using an old Warp 10 backend, it might be cityzendata.
+
+Add the warpscript.js in (src/core_plugins/timelion/server/series_function) folder.
+
+## Simple use
+
+To use warpscript use the following command: 
+
+```
+.warpscript(code="YOUR WARPSCRIPT HERE")
+```
+
+Then the FIRST element on the stack will be converted to TIMELION format. It expects to found a single GeoTime serie or a list of GTS.
+
+## Position parameter 
+
+To choose to print an element that is a different position on the WarpScript stack use the attribute position: 
+
+```
+.warpscript(code="YOUR WARPSCRIPT HERE",position=1)
+```
+
+This command will also load the first element on the stack then will converts it to TIMELION format.
+
+
+## Fit parameter
+
+Algorithm to use for fitting series to the target time span and interval. Available: average, carry, nearest, none, scale.
+
+```
+.warpscript(code="YOUR WARPSCRIPT HERE",fit="none")
+```
+
+## DataFormat parameter
+
+Add some custom series attributes to each series to plot. The api can be found on github: https://github.com/elastic/timelion/blob/master/bower_components/flot/API.md. The attributes that can added belongs to the Data Format category.
+
+```
+.warpscript(code="YOUR WARPSCRIPT HERE",dataFormat='{"shadowSize": 8}')
+```
+
+## Time interval
+
+The time interval parameter configured with timelion is push as a number variable in WarpScript (converted in base unit Time of the Warp10 platform reached).
+
+To reuse it just load it from the variable *intervalTime* somewhere in your WarpScript code: 
+
+```
+$intervalTime
+```
---
+++ warpscript.js
@@ -0,0 +1,124 @@
+//TODO: LAT/LONG/ELEV + options
+
+var _ = require('lodash');
+var Datasource = require('../lib/classes/datasource');
+var fetch = require('node-fetch');
+
+module.exports = new Datasource ('warpscript', {
+  dataSource: true,
+  args: [
+    {
+      name: 'code',
+      types: ['string', 'null'],
+      help: 'The WarpScript to plot. You can find the documentation on warp10.io.'
+    },
+    {
+      name: 'position',
+      types: ['number', 'null'],
+      help: 'Some Warp10 sources return multiple elements on the stack, which one should I use? 1 based index.'
+    },
+    {
+      name: 'dataFormat',
+      types: [ 'string', 'null'],
+      help: 'Add custom series attributes. The api can be found on github: https://github.com/elastic/timelion/blob/master/bower_components/flot/API.md'
+    }
+  ],
+  help: `
+    [experimental]
+    Pull data from Warp10 using the WarpScript code. Set "warp10:backend.url" to the URL to get the data.`,
+
+  fn: function warp10Fn(args, tlConfig) {
+
+    //
+    // Load data from the command argument
+    //
+    var config = _.defaults(args.byName, {
+      code: '',
+      position: 1,
+      interval: tlConfig.time.interval,
+      dataFormat: "NULL",
+      warp10url: tlConfig.settings['timelion:warpscript.url'],
+      headerValue: tlConfig.settings['timelion:warpscript.header']
+    });
+
+    //
+    // Split time interval (split number from letters)
+    //
+
+    var intervalArray = config.interval.match(/[a-zA-Z]+|[0-9]+/g)
+    
+    //
+    // Prepare body, storing time interval used in Timelion
+    // 
+    var prep_body = intervalArray[0] + " " + intervalArray[1] + " \'intervalTime\' STORE "
+
+    //
+    // Prepare the body adding the code
+    //
+    prep_body += config.code.toString();
+
+    //
+    // Add specific code to convert data to TimeLion format
+    // And add bucketize size to match timelion buckets
+    //
+    prep_body += " " + config.position.toString() + " PICK ";
+
+    //
+    // Try parsing dataFormat attributes
+    //
+    
+    var dataFormatAttributes = config.dataFormat;
+
+    if (config.dataFormat != "NULL") {
+      try { JSON.parse(config.dataFormat);}
+      catch(e) {
+        throw new Error('WarpScript() unsupported dataFormat: ' + config.dataFormat + '. warpscript() supports only JSON Strings');
+      }
+      dataFormatAttributes = "\'" + config.dataFormat + "\'";
+      dataFormatAttributes += " JSON-> "
+    }
+
+    //
+    // Add timelion dataFormats attributes to body
+    //
+    prep_body += dataFormatAttributes + " @orbit/timelion/convert ";
+
+    //
+    // Apply a Post on Warp 10 backend url
+    //
+    return fetch(config.warp10url, {
+
+      //
+      // Set up an HTTP(S) POST
+      // with the body containing the code given as parameter fully completed
+      //
+      method: 'POST', 
+      body: prep_body,
+    }).then(function (resp) {
+      if(!resp.ok) {
+        throw new Error('WarpScript() catched an error at line ' + resp.headers._headers['x-' + config.headerValue + '-error-line'][0] 
+          + ': ' + resp.headers._headers['x-' + config.headerValue + '-error-message'][0]);
+      }
+
+      //
+      // Get result as JSON format
+      //
+      return resp.json();
+
+    }).then(function(results) {
+
+      //
+      // Apply on result:
+      // Retun an object containing a list of series 
+      // Loaded from the user selected position on the WarpScript stack
+      //
+      // merge resulting object with user attribute
+      return {
+              type: 'seriesList',
+              list: results[0]
+      };
+    }).catch(function(err) {
+      throw new Error(err.message);
+    });
+  }
+});
